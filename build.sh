ce6be8a9a7f8e02d93704631363bb7b15ca52262
Tobias Anker <tobias.anker@kitsunemimi.moe>
2022-09-05 22:59:07 +0200

    Change: update build-script

--- build.sh
+++ build.sh
@@ -55,11 +55,24 @@ function get_required_private_repo_github () {
     NUMBER_OF_THREADS=$3
 
     # clone repo
-    git clone https://kitsudaiki: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@github.com/kitsudaiki/$REPO_NAME.git "$PARENT_DIR/$REPO_NAME"
+    git clone https://kitsudaiki:$CLONE_TOKEN@github.com/kitsudaiki/$REPO_NAME.git "$PARENT_DIR/$REPO_NAME"
     cd "$PARENT_DIR/$REPO_NAME"
     git checkout $TAG_OR_BRANCH
 
     build_kitsune_lib_repo $REPO_NAME $NUMBER_OF_THREADS
+}
+
+function download_private_repo_github () {
+    REPO_NAME=$1
+    TAG_OR_BRANCH=$2
+
+    # clone repo
+    git clone https://kitsudaiki:$CLONE_TOKEN@github.com/kitsudaiki/$REPO_NAME.git "$BUILD_DIR/$REPO_NAME"
+    git clone https://kitsudaiki:$CLONE_TOKEN@github.com/kitsudaiki/$REPO_NAME.git "$PARENT_DIR/$REPO_NAME"
+    cd "$BUILD_DIR/$REPO_NAME"
+    git checkout $TAG_OR_BRANCH
+    cd "$PARENT_DIR/$REPO_NAME"
+    git checkout $TAG_OR_BRANCH
 }
 
 
